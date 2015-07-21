16b7cb42e5b45da9aacc9c9349e2a66e9019f11c
Nikita Tretyakov <niktre@gmail.com>
2015-07-21 12:54:43 +0200

    dummy functions for derivatives

--- derivFD.c
+++ derivFD.c
@@ -9,3 +9,38 @@ void firstDer (double res, double *field, int x, int y, int dir) {
 	  res += tau_derFirst[i]*lbmodel.c[i][dir]*field[i];
 	}
 }
+
+/***********************************************************************/
+
+void secDerAA (double res, double *field, int x, int y, int dir) {
+	int i;
+	res = 0.;
+	
+	for (i=0; i<NFORCE; ++i) {
+		res += tau_derFirst[i]*lbmodel.c[i][dir]*field[i];
+	}
+}
+
+/***********************************************************************/
+
+void secDerAB (double res, double *field, int x, int y, int dir) {
+	int i;
+	res = 0.;
+	
+	for (i=0; i<NFORCE; ++i) {
+		res += tau_derFirst[i]*lbmodel.c[i][dir]*field[i];
+	}
+}
+
+/***********************************************************************/
+
+void thirdDer (double res, double *field, int x, int y, int dir) {
+	int i;
+	res = 0.;
+	
+	for (i=0; i<NFORCE; ++i) {
+		res += tau_derFirst[i]*lbmodel.c[i][dir]*field[i];
+	}
+}
+
+/***********************************************************************/
